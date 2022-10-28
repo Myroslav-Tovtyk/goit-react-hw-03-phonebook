acb06ea317ff611bdda4d8c0627b22bae44f5535
Myroslav Tovtyk <tovmyr@gmail.com>
2022-10-28 19:04:37 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,89 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from '../components/ContactForm/ContactForm';
+import { ContactList } from '../components/ContactList/ContactList';
+import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const lsd = JSON.parse(localStorage.getItem('contacts'));
+    if (!lsd) {
+      return;
+    }
+    this.setState({ contacts: lsd });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({ filter: e.target.value });
+  };
+
+  formValue = (values, actions) => {
+    const newContact = {
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    };
+
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === values.name.toLowerCase()
+      )
+    ) {
+      alert(`${values.name} is already in contacts`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, newContact],
+    }));
+    actions.resetForm();
+    console.log(this.state.contacts);
+  };
+
+  renderList = () => {
+    return this.state.filter
+      ? this.state.contacts.filter(contact =>
+          contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+        )
+      : this.state.contacts;
+  };
+
+  deleteContact = e => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== e.target.id),
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <ContactForm formValue={this.formValue} />
+        <Section title="Contacts:">
+          <ContactList
+            renderList={this.renderList()}
+            deleteContact={this.deleteContact}
+          />
+        </Section>
+        <Filter handleChange={this.handleChange} />
+      </>
+    );
+  }
+}
